8c4993bfd63b96927502737b01da5ff4a1848060
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-06-21 09:30:39 +0900

    Add another example

---
+++ src/struct_func.rs
@@ -0,0 +1,19 @@
+struct Person {
+    pub name: String,
+    pub age: u32,
+}
+
+impl Person {
+    pub fn new(name: &str, age: u32) -> Person {
+        return Person {
+            name: name.to_string(),
+            age: age,
+        };
+    }
+}
+
+fn main() {
+    //let ore = Person{ name: "上田".to_string(), age: 100046 };
+    let ore = Person::new("上田", 100046);
+    println!("おっすおら{}、{}歳。", &ore.name, &ore.age);
+}
